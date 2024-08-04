575aa453c2bbf0bf770551a711993587674564ac
Renjie Weng <renjie.weng@gmail.com>
2024-08-03 21:06:01 -0700

    add google analytics

--- load_html_script.py
+++ load_html_script.py
@@ -180,6 +180,15 @@ html_content = """
             }
         }
     </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-51069014-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-51069014-1');
+    </script>
 </head>
 <body>
     <div id="events-container">
