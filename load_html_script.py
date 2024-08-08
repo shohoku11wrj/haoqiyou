e3fc0399c80f98d5193db72419ccf72fa4dda0f2
Renjie Weng <renjie.weng@gmail.com>
2024-08-08 13:47:36 -0700

    update format of description

--- load_html_script.py
+++ load_html_script.py
@@ -140,6 +140,13 @@ html_content = """
             display: flex;
             flex-direction: column;
         }
+        .event-description {
+            white-space: pre-wrap; /* CSS3 */
+            white-space: -moz-pre-wrap; /* Mozilla */
+            white-space: -pre-wrap; /* Opera 4-6 */
+            white-space: -o-pre-wrap; /* Opera 7 */
+            word-wrap: break-word; /* Internet Explorer 5.5+ */
+        }
         .date-box {
             position: absolute;
             top: 0;
